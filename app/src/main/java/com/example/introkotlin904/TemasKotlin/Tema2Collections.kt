02cff9a6c2961fe6a5def33ac87abe136207946e
IDGS-904-21000443 <josejuanaguirregarcia4@gmail.com>
2025-05-06 19:35:36 -0600

    tema 3 if when

--- app/src/main/java/com/example/introkotlin904/TemasKotlin/Tema2Collections.kt
+++ app/src/main/java/com/example/introkotlin904/TemasKotlin/Tema2Collections.kt
@@ -10,8 +10,8 @@ package com.example.introkotlin904.TemasKotlin
 * - MutableMap
 * */
 
-fun main(){
-    // LISTAS
+/*
+* // LISTAS
     // Una lista que es inmutable. no se puede modificar
     val readOnlyFiguras : List<String> = listOf("cuadrado", "triangulo", "circulo")
     print(readOnlyFiguras.toString())
@@ -31,8 +31,22 @@ fun main(){
     println(coches.toString())
     // Un map que sí se puede modificar
     var mutableCoches = mutableMapOf("Uno" to 1, "Dos" to 2, "Tres" to 3)
-    println(mutableCoches
+    println(mutableCoches)
+* */
 
-
-    )
+fun main(){
+    val readOnlyFiguras : List<String> = listOf("cuadrado", "triangulo", "circulo")
+    println(readOnlyFiguras)
+    println("La primera figura es: ${readOnlyFiguras[0]}")
+    println("El primer elemento es: ${readOnlyFiguras.first()}")
+    println("El numero de elementos es: ${readOnlyFiguras.count()} items")
+    println("circulo: " in readOnlyFiguras)
+    println(readOnlyFiguras)
+    // readOnlyFiguras.add("pentagono")
+    var figura : MutableList<String> = mutableListOf("cuadrado2", "triangulo2", "circulo2")
+    println(figura)
+    figura.add("pentagono2")
+    println(figura)
+    figura.remove("cuadrado2")
+    println(figura)
 }
---
+++ app/src/main/java/com/example/introkotlin904/TemasKotlin/Tema3IfWhen.kt
@@ -0,0 +1,26 @@
+package com.example.introkotlin904.TemasKotlin
+
+fun main() {
+    val d : Int
+    val check : Boolean = true
+
+    if (check) {
+        d = 1
+    } else {
+        d = 2
+    }
+
+    println(d)
+
+    val d2 = if (check) 1 else 2
+    println(d2)
+
+    println("-------------------------------------")
+    println("Ingrese el sueldo del empleado: ")
+    val sueldo : Double = readln().toDouble()
+    if (sueldo > 3000) {
+        println("Debe pagar impuestos")
+    }
+
+    // Hasta aquí nos quedamos
+}
